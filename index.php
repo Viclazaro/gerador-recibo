1912a0ccee8a6dabc79a966711e07ec34c6b2d98
Yen <thearyazitah@gmail.com>
2026-04-27 20:32:56 -0300

    add linhas de preenchimento para assinatura e documentos

--- index.php
+++ index.php
@@ -1,44 +1,50 @@
 <?php
-
+/**
+ * --------------------------------------------------
+ * Gerador de Recibo com FPDF
+ * --------------------------------------------------
+ * Descrição:
+ * Este script gera um recibo em PDF utilizando a biblioteca FPDF.
+ * 
+ * Funcionalidades:
+ * - Cria um recibo de pagamento
+ * - Insere campos como nome e RG
+ * - Formata o layout com linhas para preenchimento
+ * 
+ * Autor: Victória
+ * Data: 2026
+ * --------------------------------------------------
+ */
 require_once('fpdf.php');
 
-//GERAR PÁGINAS
 $pdf = new FPDF();
+// Define margens: esquerda, topo, direita
+$pdf->SetMargins(20, 20, 20);
+
+// Define quebra automática respeitando margem inferior
+$pdf->SetAutoPageBreak(true, 20);
 $pdf->AddPage();
 
-// ESTRUTURA CÓDIGO
 $pdf->SetFont('Arial','B',16);
 $pdf->Cell(0, 10, utf8_decode('RECIBO DE PAGAMENTO'), 0, 1, 'C');
 $pdf->Ln(10);
 
-// LINHA 1: NOME
+// pega posição atual
+$x = $pdf->GetX();
+$y = $pdf->GetY();
+
+$larguraRestante = 210 - 20 - $x; // página - margem direita - posição atual
+
+$pdf->SetXY($x, $y);
+
 $pdf->SetFont('Arial','',12);
-$pdf->cell(0, 20, utf8_decode('Pelo presente, eu, '));
+$pdf->MultiCell($larguraRestante, 6, utf8_decode('Pelo presente, eu, _________________________________________, portador do RG, nº _____________________, CPF nº _______________________________. Declaro ter recebido de _____________________________________, CPF/CNPJ nº _____________________________, a importância de R$ _____________________ (valor por extenso), referente a __________________________________.'), 0, 0);
 
-$pdf->SetX(50);
-$pdf->Cell(100, 12, '', 'B', );
-$pdf->Ln(10);
-//LINHA 2: RG
-$pdf->SetFont('Arial','',12);
-$pdf->cell(0, 20, utf8_decode(' , carteira de identidade (RG), Nº '));
+$pdf->MultiCell($larguraRestante, 50,'_________________, ____/____/_______', 0, 'C' );
 
-// $pdf->SetX(10);
-// $pdf->Cell(80, 22, '', 'B');
-// $pdf->cell(0, 40, utf8_decode(' , expedida por '));
+$pdf->MultiCell($larguraRestante, 7,'______________________________', 0, 'C' );
 
-// $pdf->SetX(121);
-// $pdf->Cell(50, 22, '', 'B');
+$pdf->SetFont('Arial','B',12);
+$pdf->MultiCell($larguraRestante, 7,'Assinatura', 0, 'C' );
 
-// $pdf->cell(0, 40, utf8_decode(' , CPF Nº'));
-
-// $pdf->SetX(10);
-// $pdf->Cell(70, 32.5, '', 'B');
-
-// $pdf->cell(0, 61, utf8_decode(' , declaro ter recebido de '));
-
-// $pdf->SetX(130);
-// $pdf->Cell(50, 32.2, '', 'B');
-// $pdf->cell(0, 61, utf8_decode(' , carteira de'));
-
-//SAÍDA
 $pdf->Output();
